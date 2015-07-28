39238d4869cef06c4c4a09f2dbb0f1790d92f8a6
Alex Dunn <dunn.alex@gmail.com>
2015-07-27 22:20:41 -0700

    brew history: reduce nesting

--- cmd/brew-history.rb
+++ cmd/brew-history.rb
@@ -26,27 +26,26 @@ end
 # the dash between hash and unixtime in --format is used in .split("-") below
 git_args = %w[log --reverse --format="%H-%at" --diff-filter=AD]
 
-HOMEBREW_LIBRARY.cd do
-  ARGV.named.each do |f|
-    puts "#{Tty.em}#{f}.rb#{Tty.reset}"
-    f_args = Array.new(git_args) << "--" << "Formula/#{f}.rb"
-    commits = `git #{f_args.join(" ")}`.strip.split("\n")
-    commits.each_with_index do |elem, index|
-      # split into array with hash and time
-      commit = elem.split("-")
-      ctime = Time.at(commit[1].to_i).strftime("%Y-%m-%d")
-      if index == 0
-        puts "#{Tty.white}Added#{Tty.reset} on #{ctime} in #{commit[0]}"
+Dir.chdir HOMEBREW_LIBRARY
+ARGV.named.each do |f|
+  puts "#{Tty.em}#{f}.rb#{Tty.reset}"
+  f_args = Array.new(git_args) << "--" << "Formula/#{f}.rb"
+  commits = `git #{f_args.join(" ")}`.strip.split("\n")
+  commits.each_with_index do |elem, index|
+    # split into array with hash and time
+    commit = elem.split("-")
+    ctime = Time.at(commit[1].to_i).strftime("%Y-%m-%d")
+    if index == 0
+      puts "#{Tty.white}Added#{Tty.reset} on #{ctime} in #{commit[0]}"
+    else
+      # get the diff of tap_migrations from the commit where the formula was deleted
+      migrations = `git diff #{commit[0]}~1..#{commit[0]} -- Homebrew/tap_migrations.rb`.strip.split("\n")
+      new_tap = migrations.select { |s| s.include?("\"#{f}\"") }.to_s
+      new_tap = %r{(?<=\=\>)[^a-zA-Z]*([^\\]*\/[^\\]*)}.match(new_tap).captures[0]
+      if new_tap
+        puts "#{Tty.white}Moved#{Tty.reset} to #{new_tap} on #{ctime} in #{commit[0]}"
       else
-        # get the diff of tap_migrations from the commit where the formula was deleted
-        migrations = `git diff #{commit[0]}~1..#{commit[0]} -- Homebrew/tap_migrations.rb`.strip.split("\n")
-        new_tap = migrations.select { |s| s.include?("\"#{f}\"") }.to_s
-        new_tap = %r{(?<=\=\>)[^a-zA-Z]*([^\\]*\/[^\\]*)}.match(new_tap).captures[0]
-        if new_tap
-          puts "#{Tty.white}Moved#{Tty.reset} to #{new_tap} on #{ctime} in #{commit[0]}"
-        else
-          puts "#{Tty.white}Removed#{Tty.reset} on #{ctime} in #{commit[0]}"
-        end
+        puts "#{Tty.white}Removed#{Tty.reset} on #{ctime} in #{commit[0]}"
       end
     end
   end
